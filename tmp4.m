97478fe628c2159d0b7a4a262c3feaa5eff05c18
Gustavo Henrique <ghboliveira@hotmail.com>
2023-09-05 07:20:04 -0300

    Atualizando código

--- tmp4.m
+++ tmp4.m
@@ -1,12 +1,19 @@
+% Define array of frequencies, amplitudes, orders, and cutoffs
+freq_nyquist_fraction = 0.5; % Fraction of Nyquist frequency
+frequencies = [3000, 5000]; % Frequencies of signals (in Hz)
+amplitudes = [1, 1]; % Amplitudes of signals
+orders = [2, 2]; % Order of filters
+cutoffs = [4000, 6000]; % Cutoff frequencies of filters
+
+min_frequecy = min(frequencies);
+periods = 3; % Number of periods to plot
+
 % Define parameters
 Fs_real = 1000000; % Sampling frequency (real data)
-Fs = 10000; % Sampling frequency
-t = 0:1/Fs:0.0004; % Time vector
-t_real = 0:1/Fs_real:0.0004; % Time vector (real data)
+Fs = 100000; % Sampling frequency
+t = 0:1/Fs:periods/min_frequecy; % Time vector
+t_real = 0:1/Fs_real:periods/min_frequecy; % Time vector (real data)
 
-% Define array of frequencies and amplitudes
-frequencies = [1000, 3000, 5000]; % Frequencies of signals (in Hz)
-amplitudes = [1, 1, 1]; % Amplitudes of signals
 
 num_signals = length(frequencies);
 signals = zeros(num_signals, length(t));
@@ -26,16 +33,17 @@ end
 summed_signal = sum(signals);
 
 % Plot signals and FFTs
-figure;
+% Initialize figure
+figure('CloseRequestFcn', @(src, event) close_all_figures());
 
 for i = 1:num_signals
-    subplot(4, 4, 4*i-3);
+    subplot(num_signals, 4, 4*i-3);
     plot(t_real, signals_real(i, :), '.-');
     title(['Real Signal ', num2str(i)]);
     xlabel('Time (s)');
     ylabel('Amplitude');
     
-    subplot(4, 4, 4*i-2); % FFT plot is on the right side
+    subplot(num_signals, 4, 4*i-2); % FFT plot is on the right side
     fft_signal = fft(signals_real(i, :));
     frequencies_fft = linspace(0, Fs_real, length(fft_signal));
     plot(frequencies_fft, abs(fft_signal));
@@ -43,13 +51,14 @@ for i = 1:num_signals
     xlabel('Frequency (Hz)');
     ylabel('Magnitude');
 
-    subplot(4, 4, 4*i-1);
-    plot(t, signals(i, :), '.-');
+    subplot(num_signals, 4, 4*i-1);
+    # plot(t, signals(i, :), '.-');
+    stem(t, signals(i, :), '.');
     title(['Signal ', num2str(i)]);
     xlabel('Time (s)');
     ylabel('Amplitude');
 
-    subplot(4, 4, 4*i); % FFT plot is on the right side
+    subplot(num_signals, 4, 4*i); % FFT plot is on the right side
     fft_signal = fft(signals(i, :));
     frequencies_fft = linspace(0, Fs, length(fft_signal));
     plot(frequencies_fft, abs(fft_signal));
@@ -59,7 +68,8 @@ for i = 1:num_signals
 end
 
 % Plot summed signal and its FFT
-figure;
+% Initialize figure
+figure('CloseRequestFcn', @(src, event) close_all_figures());
 subplot(2, 1, 1);
 plot(t, summed_signal);
 title('Summed Signal');
@@ -73,5 +83,10 @@ plot(frequencies_fft_summed_signal, abs(fft_summed_signal));
 title('FFT of Summed Signal');
 xlabel('Frequency (Hz)');
 ylabel('Magnitude');
+
+% Callback function to close all figures
+function close_all_figures(main_figure)
+    exit;
+end
 
 pause;
