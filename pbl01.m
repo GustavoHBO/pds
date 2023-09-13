d360a9e1e547101d439b7bd5023529481351e57a
Antonio Mendes <raymendesjr2013@gmail.com>
2023-09-13 17:18:03 -0300

    Change filter from ideal to butter

--- pbl01.m
+++ pbl01.m
@@ -5,8 +5,6 @@ clc;
 pkg load signal
 pkg load symbolic
 
-figure('CloseRequestFcn', @(src, event) close_all_figures());
-
 w1 = 2*pi*10;
 w2 = 2*pi*50;
 w3 = 2*pi*90;
@@ -22,15 +20,22 @@ tam_yt = length(yt);
 fre = (-tam_yt/2:tam_yt/2 - 1)/1;
 fft_yt = (1/500)*fft(yt, tam_yt);
 
-filtro_low_pass = ((-1)*rectpuls(fre, 2*485) + rectpuls(fre, 2*600));
+% Filtro passa-baixa de Butterworth
+fc = 15; % Frequência de corte em Hz
+ordem = 2; % Ordem do filtro
+[b, a] = butter(ordem, fc/(Fs/2)); % Projeta o filtro
+yt_filtr = filter(b, a, yt); % Aplica o filtro ao sinal
 
-freqs_interesse = (fft_yt.*filtro_low_pass);
+##filtro_low_pass = ((-1)*rectpuls(fre, 2*485) + rectpuls(fre, 2*600));
+
+##freqs_interesse = (fft_yt.*filtro_low_pass);
 
 yt_ifft = ifft(fft_yt);
 
-yt_filtr = 500*ifft(freqs_interesse);
+##yt_filtr = 500*ifft(freqs_interesse);
 
 figure('CloseRequestFcn', @(src, event) close_all_figures());
+figure
 plot(t, yb);
 title('Sinal esperado', 'FontSize', 25);
 grid on;
@@ -38,7 +43,8 @@ xlabel('Tempo (s)', 'FontSize', 25);
 ylabel('Amplitude', 'FontSize', 25);
 legend('Sinal amostrado na frequência', 'FontSize', 25);
 
-figure('CloseRequestFcn', @(src, event) close_all_figures());
+% figure('CloseRequestFcn', @(src, event) close_all_figures());
+figure
 plot(t, yt);
 title('Sinal de entrada no tempo', 'FontSize', 25);
 grid on;
@@ -46,45 +52,45 @@ xlabel('Tempo (s)', 'FontSize', 25);
 ylabel('Amplitude', 'FontSize', 25);
 legend('Sinal amostrado na frequência', 'FontSize', 25);
 
-figure('CloseRequestFcn', @(src, event) close_all_figures());
+% figure('CloseRequestFcn', @(src, event) close_all_figures());
+##stem(fre, abs(fftshift(fft_yt)), '.', 'LineWidth', 1.5);
+##axis([-200 200 0 1]);
+##title('Sinal de entrada na frequência', 'FontSize', 25);
+##grid on;
+##xlabel('Frequência (Hz)', 'FontSize', 25);
+##ylabel('Amplitude', 'FontSize', 25);
+##legend('Sinal amostrado na frequência', 'FontSize', 25);
 
-stem(fre, abs(fftshift(fft_yt)), '.', 'LineWidth', 1.5);
-axis([-200 200 0 1]);
-title('Sinal de entrada na frequência', 'FontSize', 25);
-grid on;
-xlabel('Frequência (Hz)', 'FontSize', 25);
-ylabel('Amplitude', 'FontSize', 25);
-legend('Sinal amostrado na frequência', 'FontSize', 25);
+##% figure('CloseRequestFcn', @(src, event) close_all_figures()); figure
+##plot(fre, fftshift(filtro_low_pass));
+##axis([-200 200 0 1]);
+##title('Filtro passa-baixa ideal', 'FontSize', 25);
+##grid on;
+##xlabel('Tempo (s)', 'FontSize', 25);
+##ylabel('Amplitude', 'FontSize', 25);
+##legend('Sinal amostrado na frequência', 'FontSize', 25);
 
-figure('CloseRequestFcn', @(src, event) close_all_figures());
-plot(fre, fftshift(filtro_low_pass));
-axis([-200 200 0 1]);
-title('Filtro passa-baixa ideal', 'FontSize', 25);
-grid on;
-xlabel('Tempo (s)', 'FontSize', 25);
-ylabel('Amplitude', 'FontSize', 25);
-legend('Sinal amostrado na frequência', 'FontSize', 25);
+##% figure('CloseRequestFcn', @(src, event) close_all_figures()); figure
+##stem(fre, abs(fftshift(freqs_interesse)), '.', 'LineWidth', 1.5);
+##axis([-200 200 0 1]);
+##title('Sinal de entrada filtrado na frequência', 'FontSize', 25);
+##grid on;
+##xlabel('Frequência (Hz)', 'FontSize', 25);
+##ylabel('Amplitude', 'FontSize', 25);
+##legend('Sinal amostrado na frequência', 'FontSize', 25);
 
-figure('CloseRequestFcn', @(src, event) close_all_figures());
-stem(fre, abs(fftshift(freqs_interesse)), '.', 'LineWidth', 1.5);
-axis([-200 200 0 1]);
-title('Sinal de entrada filtrado na frequência', 'FontSize', 25);
-grid on;
-xlabel('Frequência (Hz)', 'FontSize', 25);
-ylabel('Amplitude', 'FontSize', 25);
-legend('Sinal amostrado na frequência', 'FontSize', 25);
-
-figure('CloseRequestFcn', @(src, event) close_all_figures());
+% figure('CloseRequestFcn', @(src, event) close_all_figures());
+figure
 plot(t, yt_filtr);
 axis([0 1 -1 1]);
-title('Sinal de entrada filtrado reconstruído', 'FontSize', 25);
+title('Sinal de entrada filtrado', 'FontSize', 25);
 grid on;
 xlabel('Tempo (s)', 'FontSize', 25);
 ylabel('Amplitude', 'FontSize', 25);
 legend('Sinal de entrada x(t) filtrado.', 'FontSize', 25);
 
 
-ht = (1/2)*(square(0.5*w1*t) + 1);
+ht = (1/2)*(square(4*w1*t) + 1);
 
 ys = yt_filtr.*ht;
 
@@ -102,7 +108,8 @@ ys_reconstr = ifft(fft_ys);
 yb_reconstr = 500*(ifft(ys_filtr));
 
 % subplot(3,1,1)
-figure('CloseRequestFcn', @(src, event) close_all_figures());
+% figure('CloseRequestFcn', @(src, event) close_all_figures());
+figure
 plot(t, ht);
 title('Trem de pulsos retangulares', 'FontSize', 25);
 grid on;
@@ -110,8 +117,8 @@ xlabel('Tempo (s)', 'FontSize', 25);
 ylabel('Amplitude', 'FontSize', 25);
 legend('Trem de pulsos h(t)', 'FontSize', 25);
 
-figure('CloseRequestFcn', @(src, event) close_all_figures());
-
+% figure('CloseRequestFcn', @(src, event) close_all_figures());
+figure
 plot(t, ys);
 axis([0 1 -1.1 1.1]);
 title('Sinal amostrado', 'FontSize', 25);
@@ -120,8 +127,8 @@ xlabel('Tempo (s)', 'FontSize', 25);
 ylabel('Amplitude', 'FontSize', 25);
 legend('Sinal amostrado', 'FontSize', 25);
 
-figure('CloseRequestFcn', @(src, event) close_all_figures());
-
+% figure('CloseRequestFcn', @(src, event) close_all_figures());
+figure
 stem(fre2, abs(fftshift(fft_ys)), '.', 'LineWidth', 1.5);
 axis([-200 200 0 1.1]);
 title('Sinal amostrado na frequência', 'FontSize', 25);
@@ -129,8 +136,9 @@ grid on;
 xlabel('Frequência (Hz)', 'FontSize', 25);
 ylabel('Amplitude', 'FontSize', 25);
 legend('Sinal amostrado na frequência', 'FontSize', 25);
-figure('CloseRequestFcn', @(src, event) close_all_figures());
 
+% figure('CloseRequestFcn', @(src, event) close_all_figures());
+figure
 plot(fre2, fftshift(filtro_reconstr));
 axis([-200 200 0 1]);
 title('Filtro de reconstrução', 'FontSize', 25);
@@ -139,7 +147,8 @@ xlabel('Tempo (s)', 'FontSize', 25);
 ylabel('Amplitude', 'FontSize', 25);
 legend('Filtro de reconstrução δ(t)', 'FontSize', 25);
 
-figure('CloseRequestFcn', @(src, event) close_all_figures());
+% figure('CloseRequestFcn', @(src, event) close_all_figures());
+figure
 stem(fre2, abs(fftshift(ys_filtr)), '.', 'LineWidth', 1.5);
 axis([-200 200 0 1.1]);
 title('Sinal amostrado filtrado na frequência', 'FontSize', 25);
@@ -148,11 +157,11 @@ xlabel('Frequência (Hz)', 'FontSize', 25);
 ylabel('Amplitude', 'FontSize', 25);
 legend('Sinal amostrado e filtrado na frequência', 'FontSize', 25);
 
-figure('CloseRequestFcn', @(src, event) close_all_figures());
-
+% figure('CloseRequestFcn', @(src, event) close_all_figures());
+figure
 plot(t, yb_reconstr);
 hold on;
-plot(t, sin(w1*t+0.5));
+plot(t, yb);
 axis([0 1 -1 1]);
 xlabel('Tempo (s)', 'FontSize', 25);
 ylabel('Amplitude', 'FontSize', 25);
@@ -165,5 +174,3 @@ function close_all_figures(main_figure)
     % close all;
     exit;
 end
-
-pause;
